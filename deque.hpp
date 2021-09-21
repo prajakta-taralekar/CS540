7cbb4e09adacc097f138ebfa7cb43852c503064c
prajakta <76111944+prajakta-taralekar@users.noreply.github.com>
2021-09-20 21:28:00 -0400

    Update deque.hpp
    Iterator functions

--- deque.hpp
+++ deque.hpp
@@ -9,16 +9,18 @@
 #define DEF_CAP 8
 
 #define Deque_DEFINE(t)                                                        
-  struct Deque_##t##_Iterator;                                                 
-  struct Deque_##t;                                                            
-  struct Deque_##t##_Iterator {                                                
+struct Deque_##t##_Iterator;                                                 
+struct Deque_##t;                                                            
+struct Deque_##t##_Iterator 
+  {                                                
     unsigned int index;                                                        
     Deque_##t *deq;                                                            
     void (*inc)(Deque_##t##_Iterator *);                                       
     void (*dec)(Deque_##t##_Iterator *);                                       
     t &(*deref)(Deque_##t##_Iterator *);                                       
   };                                                                           
-  struct Deque_##t {                                                           
+struct Deque_##t 
+  {                                                           
     unsigned int cap;                                                          
     unsigned int start_i;                                                      
     unsigned int offset;                                                       
@@ -41,4 +43,30 @@
     void (*clear)(Deque_##t *);                                                
     void (*dtor)(Deque_##t *);                                                 
     void (*sort)(Deque_##t *, Deque_##t##_Iterator, Deque_##t##_Iterator);     
-  };  
+  };
+
+/* Deque_##t##_Iterator functions */                                         
+void inc(Deque_##t##_Iterator *it) 
+  {
+    it->index++; 
+  }                          
+void dec(Deque_##t##_Iterator *it) 
+  { 
+    it->index--; 
+  }                          
+t &deref(Deque_##t##_Iterator *it) 
+  {
+    return it->deq->at(it->deq, it->index);                                    
+  }                                                                            
+bool Deque_##t##_Iterator_equal(const Deque_##t##_Iterator &it1, const Deque_##t##_Iterator &it2) 
+  {           
+    return (it1.index == it2.index) && (it1.deq == it2.deq);                   
+  }                                                                            
+void Deque_##t##_Iterator_ctor(Deque_##t##_Iterator *it, Deque_##t *deq, unsigned int index) 
+  {                         
+    it->index = index;                                                         
+    it->deq = deq;                                                             
+    it->inc = &inc;                                                            
+    it->dec = &dec;                                                            
+    it->deref = &deref;                                                        
+  }       
